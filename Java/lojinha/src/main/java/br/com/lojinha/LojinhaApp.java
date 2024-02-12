df5e2cc42b7d7fc5a7070c83b721204908f7bf16
Symon Barreto <symon@icomp.ufam.edu.br>
2024-02-12 19:46:11 -0400

    Criando classes em Java

--- Java/lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
+++ Java/lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,17 +1,30 @@
 package br.com.lojinha;
 
+import br.com.lojinha.pojo.Produto;
+
 public class LojinhaApp {
     public static void main(String[] args) {
-        System.out.println("Lojinha App!!!");
-        String nome = "Tipo String";
-        Integer numero = 24;
-        Double decimal = 1.0;
-        Boolean verdadeiro = true;
+        Produto novoProduto = new Produto();
 
-        System.out.println(nome);
-        System.out.println(numero);
-        System.out.println(decimal);
-        System.out.println(verdadeiro);
+        novoProduto.nome = "Playstation 5";
+        novoProduto.valor = 3499.99;
+        novoProduto.tamanho = "Médio";
+        novoProduto.marca = "Sony";
+        novoProduto.itensInclusos = "2 controles e 3 jogos";
+
+        System.out.println(novoProduto.nome);
+
+
+//        System.out.println("Lojinha App!!!");
+//        String nome = "Tipo String";
+//        Integer numero = 24;
+//        Double decimal = 1.0;
+//        Boolean verdadeiro = true;
+//
+//        System.out.println(nome);
+//        System.out.println(numero);
+//        System.out.println(decimal);
+//        System.out.println(verdadeiro);
 
     }
 }
---
+++ Java/lojinha/src/main/java/br/com/lojinha/pojo/Produto.java
@@ -0,0 +1,10 @@
+package br.com.lojinha.pojo;
+
+public class Produto {
+    public String nome;
+    public String marca;
+    public Double valor;
+    public String itensInclusos;
+    public String tamanho;
+
+}
