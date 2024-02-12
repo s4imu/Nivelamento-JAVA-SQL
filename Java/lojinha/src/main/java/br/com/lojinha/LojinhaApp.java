5260ddf36c25802931226c26ac110d4d819ed7df
Symon Barreto <symon@icomp.ufam.edu.br>
2024-02-12 19:29:07 -0400

    Variaveis em Java

---
+++ Java/lojinha/src/main/java/br/com/lojinha/LojinhaApp.java
@@ -0,0 +1,17 @@
+package br.com.lojinha;
+
+public class LojinhaApp {
+    public static void main(String[] args) {
+        System.out.println("Lojinha App!!!");
+        String nome = "Tipo String";
+        Integer numero = 24;
+        Double decimal = 1.0;
+        Boolean verdadeiro = true;
+
+        System.out.println(nome);
+        System.out.println(numero);
+        System.out.println(decimal);
+        System.out.println(verdadeiro);
+
+    }
+}
